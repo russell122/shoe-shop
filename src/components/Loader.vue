c849177d2ba910764de4e8b4c23f51abd2b8ee58
russell122 <romicck@mail.ru>
2024-06-04 16:01:02 +0300

    Компонент Loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+
+import { useOtherStore } from '@/stores/other.js'
+
+const otherStore = useOtherStore();
+
+const { overlay } = storeToRefs(otherStore)
+
+</script>
+
+<template>
+	<v-overlay
+			:model-value="overlay"
+			class="align-center justify-center"
+	>
+		<v-progress-circular
+				color="#3CC755"
+				size="64"
+				indeterminate
+		></v-progress-circular>
+	</v-overlay>
+</template>
+
+<style scoped lang="scss">
+
+</style>
