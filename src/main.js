b7cc3e14dbc8c97b520a738a07170f4f08eccc86
russell122 <romicck@mail.ru>
2024-06-04 16:09:03 +0300

    vuetify и другие либы

--- src/main.js
+++ src/main.js
@@ -7,13 +7,32 @@ import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import 'primeicons/primeicons.css'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+
 import App from './App.vue'
 import router from './router'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi', // This is already the default value - only for display purposes
+    },
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue);
+app.use(vuetify);
+app.use(autoAnimatePlugin);
 
 app.mount('#app')
