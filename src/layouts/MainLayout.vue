038b599f73dd00d9b0c2730038f588ffd1edc4bf
russell122 <romicck@mail.ru>
2024-11-26 18:12:24 +0300

    layouts, роутер

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+import {RouterView} from "vue-router";
+</script>
+
+<template>
+	<Header/>
+
+	<router-view></router-view>
+	<Footer/>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import MainLayout from '../layouts/MainLayout.vue'
+import SimpleLayout from '../layouts/SimpleLayout.vue'
+
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Auth from '../views/Auth.vue'
 import History from '../views/History.vue'
 import Registration from '../views/Registration.vue'
-import AuthTest from '../views/AuthTest.vue'
+import AuthTest from '../testСomponents/views/AuthTest.vue'
 import NotFound from '../views/NotFound.vue'
+import DataRetrievalError from '../views/DataRetrievalError.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      { path: '', name: 'home', component: Home },
+      { path: 'history', name: 'history', component: History },
+      { path: 'about', name: 'about', component: About },
+      { path: 'auth', name: 'auth', component: Auth },
+      { path: 'authTest', name: 'authTest', component: AuthTest },
+      { path: 'registration', name: 'registration', component: Registration },
+    ],
+  },
+  {
+    path: '/dataRetrievalError',
+    component: SimpleLayout,
+    children: [
+      { path: '', name: 'dataRetrievalError', component: DataRetrievalError }
+    ]
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: SimpleLayout,
+    children: [
+      { path: '', name: 'NotFound', component: NotFound }
+    ]
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/history',
-      name: 'history',
-      component: History
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    },
-    {
-      path: '/auth',
-      name: 'auth',
-      component: Auth
-    },
-    {
-      path: '/authTest',
-      name: 'authTest',
-      component: AuthTest
-    },
-    {
-      path: '/registration',
-      name: 'registration',
-      component: Registration
-    },
-    {
-      path: '/:pathMatch(.*)*', // Это обрабатывает все неизвестные пути
-      name: 'NotFound',
-      component: NotFound
-    }
-  ]
-})
+  routes,
+});
 
-export default router
+export default router;
