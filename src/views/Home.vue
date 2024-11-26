c4451c9e18c6c55bc16b9f892896ca0b28a800d6
russell122 <romicck@mail.ru>
2024-11-26 18:19:33 +0300

    Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,36 @@
 	import { storeToRefs } from 'pinia'
 
 	import Slider from '@/components/Slider.vue'
+	import AllShoes from "@/components/AllShoes.vue";
+
 	import { useOtherStore } from '@/stores/other.js'
-	import Cards from "@/components/Cards.vue";
+	import {watch} from "vue";
+	import {useRouter} from "vue-router";
 
 	const otherStore = useOtherStore();
 
-	const { sliderData } = storeToRefs(otherStore)
+	const { sliderData, dataRetrievalError } = storeToRefs(otherStore)
+
+	const router = useRouter();
+
+	watch(
+			() => dataRetrievalError.value,
+			(newValue) => {
+				if(newValue){
+					router.push('/dataRetrievalError')
+				}
+			},
+	);
+
+
 
 </script>
 
 <template>
-	<div class="container">
-		<Slider :data="sliderData"/>
-		<Cards/>
+	<div class="home">
+		<div class="container">
+				<Slider :data="sliderData"/>
+				<AllShoes/>
+		</div>
 	</div>
 </template>
