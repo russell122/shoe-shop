852888e210552f97d7ba3e026fb5300c19b338a3
russell122 <romicck@mail.ru>
2024-11-26 18:17:25 +0300

    DataRetrievalError

---
+++ src/views/DataRetrievalError.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+	<v-empty-state
+			headline="Упс, сервис не отвечает"
+			title="Попробуйте позже, мы занимаемся устранением неполадки"
+			text=""
+			image="/destr_cable.jpg"
+	></v-empty-state>
+</template>
+
+<style lang="scss">
+
+</style>
