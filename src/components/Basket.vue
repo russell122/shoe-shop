d03fd777511624bde4413aa074db2ad86213b18f
russell122 <romicck@mail.ru>
2025-04-09 12:34:34 +0300

    Добавлена минимальная корзина

--- src/components/Basket.vue
+++ src/components/Basket.vue
@@ -1,23 +1,39 @@
 <script setup>
+import { storeToRefs } from 'pinia';
 
+import { useOtherStore } from '@/stores/other.js';
+
+const otherStore = useOtherStore();
+
+const { basketsData } = storeToRefs(otherStore);
 </script>
 
 <template>
-	<div class="basket">
-		<h1>Корзина</h1>
-	</div>
+  <div class="basket">
+    <h1>Корзина</h1>
+    <div>
+      <div v-for="basket in basketsData" :key="basket.id">
+        <p>{{ basket.title }}</p>
+      </div>
+    </div>
+  </div>
 
 </template>
 
 <style scoped>
-	.basket {
-		position: fixed;
-		top: 0;
-		right: 0;
-		z-index: 100001;
-		background-color: white;
-		width: 500px;
-		height: 100%;
+.basket {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 100001;
+  background-color: white;
+  width: 500px;
+  height: 100%;
+}
 
-	}
+@media screen and (max-width: 576px) {
+  .basket {
+    width: 90%;
+  }
+}
 </style>
