1b0b1f8f267a07185d83e2d190814bf769c6f00b
russell122 <romicck@mail.ru>
2025-04-09 12:37:55 +0300

    прокидываю данные из сервисов

--- src/components/AllShoes.vue
+++ src/components/AllShoes.vue
@@ -1,38 +1,39 @@
 <script setup>
-	import FilterSortSearchCard from "@/components/FilterSortSearchCard.vue";
-	import Cards from "@/components/Cards.vue";
+import FilterSortSearchCard from '@/components/FilterSortSearchCard.vue';
+import Cards from '@/components/Cards.vue';
 
-	import { storeToRefs } from 'pinia'
+import { storeToRefs } from 'pinia';
 
-	import { useOtherStore } from '@/stores/other.js'
+import { useOtherStore } from '@/stores/other.js';
 
-	const otherStore = useOtherStore();
+const otherStore = useOtherStore();
 
-	const { productsData } = storeToRefs(otherStore)
+const { productsData, basketsData, bookmarkedData } = storeToRefs(otherStore);
 </script>
 
 <template>
-	<div class="all-shoes">
-		<div class="all-shoes__wrap">
-			<div class="all-shoes__descr">
-				<h1>Все кроссовки</h1>
-			</div>
+  <div class="all-shoes">
+    <div class="all-shoes__wrap">
+      <div class="all-shoes__descr">
+        <h1>Все кроссовки</h1>
+      </div>
 
-			<FilterSortSearchCard/>
-			<Cards :cards="productsData"/>
+      <FilterSortSearchCard />
+      <Cards :cards="productsData" :basketCards="basketsData" :bookmarkedCards="bookmarkedData" />
 
-		</div>
-	</div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-	.all-shoes{
-		&__descr{
-			margin-bottom: 30px;
-			h1{
-				font-size: 32px;
-				font-weight: 700;
-			}
-		}
-	}
+.all-shoes {
+  &__descr {
+    margin-bottom: 30px;
+
+    h1 {
+      font-size: 32px;
+      font-weight: 700;
+    }
+  }
+}
 </style>
