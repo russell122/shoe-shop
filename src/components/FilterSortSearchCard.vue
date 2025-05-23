804469a8628bed513571ecc4f4aacb4689c398b1
russell122 <romicck@mail.ru>
2025-05-23 17:40:36 +0300

    Начальный компонент сортировки

---
+++ src/components/FilterSortSearchCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+	import { ref } from 'vue'
+
+	const filterTypes = ref([
+		{type: 'Сначала дешевые', value: 'cheap', id: 1},
+		{type: 'Сначала дорогие', value: 'darlings', id: 2},
+		{type: 'По алфавиту', value: 'alphabet', id: 3},
+	])
+	const selectFilterTypes = ref()
+</script>
+
+<template>
+	<div class="all-shoes__interaction">
+		<v-autocomplete
+				label="Тип сортировки"
+				v-model="selectFilterTypes"
+				:items="filterTypes"
+				item-title="type"
+				item-value="value"
+		></v-autocomplete>
+	</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
